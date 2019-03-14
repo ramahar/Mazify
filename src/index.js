5ab3ebfeffc604a7009a113585b41ed1cd6cd93a
Rayyan <rmahar@ucsd.edu>
2019-03-13 17:21:42 -0700

    Add Canvas and basic styling

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ function component() {
   let element = document.createElement('div');
 
   // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  // element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
   return element;
 }
