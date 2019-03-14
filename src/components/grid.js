b49d348ca089532717e04afc9cd23dfe372a80da
Rayyan <rmahar@ucsd.edu>
2019-03-13 19:19:06 -0700

    Add node and grid class

---
+++ src/components/grid.js
@@ -0,0 +1,51 @@
+import Node from './node';
+
+class Grid {
+  constructor(width, height) {
+    this.array = [...Array(width).keys()].map(i => Array(height));
+    this.width = width;
+    this.height = height;
+    this.populateArray();
+    this.root = [];
+  }
+
+  populateArray() {
+    for (var i = 0; i < this.width; i++) {
+      for (var j = 0; j < this.height; j++) {
+        this.array[i][j] = new Node([i, j], null);
+      }
+    }
+  }
+
+  contains(x,y) {
+    return ((x >= 0 && x < this.width) && (y >= 0 && y < this.height));
+  }
+
+  isOpenAt(x,y) {
+    if (x === this.root[0] && y === this.root[1]) return false;
+    return this.contains(x,y) && !this.array[x][y].parent;
+  }
+
+  intersectsPath(x, y) {
+    const close = this.array[x][y].candNeighbors;
+    let count = 0;
+    for (var i = 0; i < close.length; i++) {
+      const x0 = close[i][0];
+      const y0 = close[i][1];
+      if (this.contains(x0,y0) && this.array[x0][y0].type === "path"){
+        count += 1;
+      }
+    }
+    if (count > 2) return true;
+  }
+
+  fillSquare(node) {
+    this.array[node.x][node.y] = node;
+  }
+
+  nodes() {
+    return [].concat.apply([], this.array);
+  }
+}
+
+export default Grid;
